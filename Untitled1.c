54015e311e6d7a2ad86db28d6dcab97a025d4aea
GiaBao-bacon <nguyengiabao102006bacon@gmail.com>
2025-05-05 11:46:59 +0700

    upload bài 2

--- Untitled1.c
+++ Untitled1.c
@@ -1,8 +1,11 @@
-#include<stdio.h>
-int main(){
-	int nameJin[50];
-	printf("Moi ban nhap ten:");
-	scanf("%s",&nameJin);
-	printf("Xin chao %s",nameJin);
-	return 0;
+#include <stdio.h>
+
+int main() {
+    float celsius, fahrenheit;
+    printf("Nhap nhiet do C: ");
+    scanf("%f", &celsius);
+    fahrenheit = (celsius * 9 / 5) + 32;
+    printf("%.2f do C = %.2f do F\n", celsius, fahrenheit);
+    return 0;
 }
+
